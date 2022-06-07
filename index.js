36fa54074d0abb8e435feaa694c6384be2e00de0
cartory <xXcartoryXx@gmail.com>
2022-06-06 23:39:16 -0400

    update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { io } = require('socket.io-client')
 const app = require('./src/app')
 const recipeService = require('./src/services/recipe.service')
 
-const socket = io('ws://localhost:3000', {
+const socket = io(process.env.HOST_MS_RESTAURANT, {
 	transports: ['websocket']
 })
 
@@ -32,7 +32,7 @@ socket.on("recipe-request", data => {
 	}
 })
 
-app.listen(() => {
+app.listen(process.env.PORT || 3000, () => {
 	console.log(new Date())
 	console.log(`Server running on \x1b[33mhttp://${process.env.HOST}:${process.env.PORT}\x1b[0m`)
 })
